b45fec7846fafb3019dd2ea79527e913cbb70da3
yuzo <spacemajestic@yahoo.co.jp>
2023-03-18 18:01:47 +0900

    display image

--- script.js
+++ script.js
@@ -10,3 +10,72 @@ const options ={
         easing:'ease'
 }
 heading.animate(keyframes,options);
+
+
+ //get image.json
+var nogizka_blog_images = 'global';
+var tags_info = [];
+let requestURL = 'https://spa.microcms.io/api/v1/blogs/13itqumhf4';
+
+function deltags(obj) {
+    work_str = obj.replace(/<br>/g, "\n").replace(/<p>/g,"").replace(/<\/p>/g,"").replace(/&nbsp;/g," ");
+    return(work_str);
+  }
+
+function makeImageList(obj) {
+  var i = 0;
+    work_image_list = [];
+    for (let key in nogizka_blog_images){
+        // 出力件数を絞って、開発する。
+        i = i +1;
+        if (i >3) {break;}
+        if (key != '五百城茉央') {continue;}
+
+        // console.log(key);
+        // console.log(nogizka_blog_images[key]);
+        // console.log(nogizka_blog_images[key]['page_info']);
+        for (numPage in nogizka_blog_images[key]['page_info']){
+          // console.log(nogizka_blog_images[key]['page_info'][numPage]['title']);
+          for (numImage in nogizka_blog_images[key]['page_info'][numPage]['image_urls']){
+            workImageUrl = nogizka_blog_images[key]['page_info'][numPage]['image_urls'][numImage];
+            // console.log(workImageUrl);
+            work_image_list.push(workImageUrl);
+          }
+        }
+    }
+    return(work_image_list);
+ }
+ function makeImageTag(imgList) {
+  const dev_blog_images = document.querySelector('.blog_images');
+    
+    for (i in imgList){
+      let img_element = document.createElement('img');
+      img_element.src = imgList[i]; // 画像パス
+      dev_blog_images.appendChild(img_element);
+    }
+ }
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+//
+request.setRequestHeader('X-MICROCMS-API-KEY', 'zQziGptrT1wUApcuwKTjzKLRMVPturQvFouy'); 
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const re_json = request.response;
+    nogizka_blog_images = JSON.parse(deltags(re_json.content));
+    image_list = makeImageList(nogizka_blog_images);
+    // console.log(image_list);
+    makeImageTag(image_list);
+    
+
+
+  }
+// for (let info in nogizka_blog_images){
+//   var work_list = [];
+//   work_list.push(info[]);
+// }
+
+console.log('test');
+// console.log(nogizka_blog_images);
+
+// tag作成
